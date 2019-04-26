207e86be4ff946338a816cebc709c28094474723
nimasalemahim <nimasalemahim@gmail.com>
2019-04-26 17:55:25 +0430

    all2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("yes");
 	    System.out.println("no");
-	    System.out.println("fdfd");
+	    System.out.println("fdfdsdsaf");
     }
 }
